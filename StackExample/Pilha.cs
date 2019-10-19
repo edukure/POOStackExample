e5e83ff70b540d1fcce9924e4c0763e576932e0c
Eduardo Tioma <tioma.eduardo@gmail.com>
2019-10-19 16:20:52 -0300

    Push to github

---
+++ StackExample/Pilha.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackExample
+{
+    public class Pilha<T>
+    {
+        private List<T> Lista { get; set; }
+        public T Topo { get; set; }
+        public int Count { get; private set; } = 0;
+
+
+        public Pilha()
+        {
+            Lista = new List<T>();
+        }
+
+        /// <summary>
+        /// Remove o item no topo da pilha
+        /// </summary>
+        public void Pop()
+        {
+            try
+            {
+                Lista.RemoveAt(Count - 1);
+                Count--;
+            }
+            catch
+            {
+                Console.WriteLine("Pilha vazia");
+            }
+        }
+       
+        /// <summary>
+        /// Adiciona um item ao topo da pilha
+        /// </summary>
+        /// <param name="item"></param>
+        public void Push(T item)
+        {
+            Lista.Add(item);
+            Count++;
+            Topo = item;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder itensPilha = new StringBuilder();
+            foreach(var item in Lista)
+            {
+                itensPilha.Insert(0,item.ToString() + '\n');
+            }
+            return itensPilha.ToString();
+        }
+
+        //Maneira alternativa de printar a lista
+        public void PrintPilha()
+        {
+            List<T> listaInvertida = Lista;
+            listaInvertida.Reverse();
+            listaInvertida.ForEach(item => Console.WriteLine(item));
+        }
+    }
+}
---
+++ StackExample/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace StackExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo com Pilha<T> Criado utilizando List<T>");
+            Pilha<int> PilhaInt = new Pilha<int>();
+            PilhaInt.Push(1);
+            PilhaInt.Push(3);
+            PilhaInt.Push(5);
+            PilhaInt.Push(7);
+            PilhaInt.Push(9);
+            Console.WriteLine(PilhaInt.ToString());
+
+            Console.WriteLine();
+            //Removendo último item colocado
+            Console.WriteLine("Removendo último item colocado");
+            PilhaInt.Pop();
+            Console.WriteLine("Pilha Atualizada:");
+            Console.WriteLine(PilhaInt.ToString());
+            //PilhaInt.PrintPilha(); outra maneira de printar a pilha
+
+            Console.ReadLine();
+
+
+            Console.Clear();
+            Console.WriteLine("Exemplo de Pilha com a classe Stack do System.Collections"); 
+
+            //Instanciando uma Stack
+            Stack Stack = new Stack();
+            //Também existe a classe Stack<T> do System.Collections.Generic
+            //Apenas Stack recebe objetos de todos os tipos
+            //Stack<T> só pode conter objetos do tipo T
+
+            //Adicionando itens à pilha
+            Stack.Push(1);
+            Stack.Push(3);
+            Stack.Push(5);
+            Stack.Push(7);
+            Stack.Push(9);
+            PrintStack(Stack);
+
+            Console.WriteLine();
+            //Removendo último item colocado
+            Console.WriteLine("Removendo último item colocado");
+            Stack.Pop();
+            Console.WriteLine("Pilha Atualizada:");
+            PrintStack(Stack);
+
+            Console.ReadLine();
+
+        }
+
+        public static void PrintStack(Stack stack)
+        {
+            foreach(var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
